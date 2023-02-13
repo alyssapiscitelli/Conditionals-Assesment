ee7d710f962cb646273d9fe528fb4949d66d57f6
alyssapiscitelli <122324506+alyssapiscitelli@users.noreply.github.com>
2023-02-13 17:45:42 -0500

    conditionals assesment
    fixed computerChoice variable

--- attributes.js
+++ attributes.js
@@ -1,16 +1,30 @@
 
 let userChoice= prompt ("Heads or Tails?")
-rand= Math.round(Math.random())
-if(userChoice!=rand){
- document.write ("Sorry, the coin flip landed on " + rand)
+const rand= Math.round(Math.random())
+let computerChoice; 
+if(rand==0) { 
+    computerChoice= 'heads'
+ }
+ else {
+    computerChoice= 'tails'
+ }
+
+if(userChoice!=computerChoice){
+ alert ("Sorry, the coin flip landed on " + computerChoice)
 }
-else {document.write("You guessed right! The coin flip landed on " + rand)}
+else {
+    alert("You guessed right! The coin flip landed on " + computerChoice)
+}
 
 const birthYear= prompt('What year were you born?')
 if ((2023-birthYear)<21)
-{alert("Sorry, you are not old enough to drink in the US");}
-else if ((2023-birthYear)==21) { alert("You are old enough to drink in the US...barely"); 
-    
-} else { alert("You are old enough to drink in the US");
+{
+    alert("Sorry, you are not old enough to drink in the US");
+}
+else if ((2023-birthYear)==21) { 
+    alert("You are old enough to drink in the US...barely");    
+}
+ else { 
+    alert("You are old enough to drink in the US");
     
 }
